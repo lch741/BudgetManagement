66007398ecdc92b707b84ecfffa4474ab6b31910
Chenghong Li <lch741@foxmail.com>
2026-01-16 23:03:14 +1300

    Add transaction API functions for CRUD operations

---
+++ frontend/src/api/transaction.api.ts
@@ -0,0 +1,24 @@
+import api from './axios'
+import type { Transaction,CreateTransaction,TransactionQuery } from "../types/transaction";
+
+
+
+export function getTransactions(query?:TransactionQuery){
+    return api.get<Transaction[]>('/Transaction',{params:query})
+}
+
+export function getTransactionById(id:number){
+    return api.get<Transaction>(`/Transaction/${id}`)
+}
+
+export function deleteTransaction(id:number){
+    return api.delete(`/Transaction/${id}`)
+}
+
+export function createTransaction(Transaction:CreateTransaction){
+    return api.post('/Transaction',{Transaction})
+}
+
+export function updateCategory(id:number,Transaction:CreateTransaction){
+    return api.put(`/Transaction/${id}`,{Transaction})
+}
