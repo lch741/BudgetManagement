8784dbcb4e52f1944b9f8d4f12c35270c08a4eb5
Chenghong Li <lch741@foxmail.com>
2026-01-18 22:05:38 +1300

    Add router setup to main application and create router file

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,10 +3,12 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
+import router from "./router/router";
 
 const app = createApp(App);
 app.use(Toast,{position:POSITION.TOP_RIGHT});
 const pinia = createPinia();
+app.use(router)
 app.use(pinia);
 app.mount('#app');
 
