2ee455a441a731b3c9f027de28d299902dc7503b
Chenghong Li <lch741@foxmail.com>
2026-01-19 22:53:55 +1300

    Add router configuration with authentication guards

---
+++ frontend/src/router/router.ts
@@ -0,0 +1,51 @@
+import { createRouter,createWebHistory } from "vue-router";
+import { useAuthStore } from "../stores/auth.store";
+
+const routes = [
+    {
+        path:'/login',
+        name:'login',
+        component:LoginView,
+    },
+    {
+        path:'/',
+        redirect:'/transactions',
+        meta:{requireAuth:true},
+        children:[
+            {
+            path:'transactions',
+            name:'transactions',
+            component:TransactionsView,
+            meta:{requireAuth:true},
+            },
+            {
+            path:'categories',
+            name:'categories',
+            component:CategoriesView,
+            meta:{requireAuth:true},
+            },
+        ]
+    }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+router.beforeEach((to)=>{
+    const auth = useAuthStore();
+    if(!auth.user&&localStorage.getItem('token')){
+        auth.init()
+    }
+
+    if(to.meta.requireAuth&&auth.isLoggedIn===false){
+        return {name:'login'}
+    }
+
+    if(to.name==='login'&&auth.isLoggedIn===true){
+        return {name:'transactions'}
+    }
+})
+
+export default router
