695299cd1ce5227bc0d8132012e1d6f57c1a15a7
Chenghong Li <lch741@foxmail.com>
2026-01-29 00:04:41 +1300

    Setup Tailwind CSS and enhance styling

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,26 @@
 import Toast, { POSITION } from "vue-toastification";
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import './style.css'
 import App from './App.vue'
 import router from "./router/router";
+import './assets/test.css'
+import "vue-toastification/dist/index.css"
 
 const app = createApp(App);
-app.use(Toast,{position:POSITION.TOP_RIGHT});
+app.use(Toast, {
+  position: POSITION.TOP_RIGHT,
+  timeout: 3000,          
+  closeOnClick: true,     
+  pauseOnFocusLoss: true, 
+  pauseOnHover: true,     
+  draggable: true,
+  draggablePercent: 0.6,
+  showCloseButtonOnHover: false,
+  hideProgressBar: false,
+  newestOnTop: true,
+  maxToasts: 5,
+  toastClassName: "rounded-lg !text-white !bg-blue-600 shadow-md px-4 py-2",
+});
 const pinia = createPinia();
 app.use(router)
 app.use(pinia);
