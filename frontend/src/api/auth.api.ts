28dd5fc77f5419d1a439aa7f68baf9604e03cdfb
Chenghong Li <lch741@foxmail.com>
2026-02-02 22:51:58 +1300

    Fix formatting in API calls for login and registration functions

--- frontend/src/api/auth.api.ts
+++ frontend/src/api/auth.api.ts
@@ -4,13 +4,13 @@ import api from './axios'
 import type { AuthResponse, LoginRequest, RegisterRequest } from "../types/auth";
 
 export function logginApi(data:LoginRequest){
-    return api.post<AuthResponse>('/account/login',{
+    return api.post<AuthResponse>('/account/login',
         data
-    })
+    )
 }
 
 export function registerApi(data:RegisterRequest){
-    return api.post<AuthResponse>('/account/register',{
+    return api.post<AuthResponse>('/account/register',
         data
-    })
+    )
 }
