a5c9eca83246dce775231f21d9188aa710a4e2f7
Chenghong Li <lch741@foxmail.com>
2026-02-10 23:30:34 +1300

    Add TransactionsView component with initial setup

---
+++ frontend/src/views/TransactionsView.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import { ref,onMounted } from 'vue';
+import { getTransactions, getTransactionById, deleteTransaction, createTransaction,updateTransaction } from '../api/transaction.api';
+import type { Transaction,CreateTransaction,TransactionQuery } from "../types/transaction";
+import { useToast } from 'vue-toastification';
+
+const transactions = ref<Transaction[]>([])
+const toast = useToast()
+
+
+
+
+
+
+
+
+
+
+
+</script>
+
+
+<template>
+    <div>
+
+    </div>
+</template>
